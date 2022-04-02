54c9e1adc5875ff7ecc3458587a0fd9c422bf020
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-04-02 22:35:47 +0200

    requirements Mad Libs Game

---
+++ Mad libs game.py
@@ -0,0 +1,15 @@
+"""
+https://www.freecodecamp.org/news/python-projects-junior-developers/
+
+Mad libs game
+Ask the user for an input.
+
+This could be anything such as a name, an adjective, a pronoun or even an action. Once you get the input,
+you can rearrange it to build up your own story.
+
+Here's a youtube tutorial on mad libs in Python.
+And example code on Github.
+
+"""
+
+
