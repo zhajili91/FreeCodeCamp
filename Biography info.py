887baf2a2b035df7972b72b7ea8bf5702896e0a6
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-04-20 17:24:29 +0200

    v0.1 Biography game

---
+++ Biography info.py
@@ -0,0 +1,18 @@
+"""
+https://www.freecodecamp.org/news/python-projects-junior-developers/
+
+Ask a user for their personal information one question at a time. Then check that the information they entered is
+valid. Finally, print a summary of all the information they entered back to them.
+
+Example: What is your name? If the user enters * you prompt them that the input is wrong, and ask them to enter a
+valid name.
+
+At the end you print a summary that looks like this:
+
+- Name: John Doe
+- Date of birth: Jan 1, 1954
+- Address: 24 fifth Ave, NY
+- Personal goals: To be the best programmer there ever was.
+
+"""
+
