05cb26914cad3158e678e3d67f1d20012afefd43
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-04-21 15:19:30 +0200

    v0.1 acronym

---
+++ What's my acronym?.py
@@ -0,0 +1,7 @@
+"""
+Ask the user to enter the full meaning of an organization or concept, and you'll provide the acronym to the user.
+For example:
+Input -> As Soon As Possible. Output -> ASAP.
+Input -> World Health Organization. Output -> WHO.
+Input -> Absent Without Leave. Output -> AWOL.
+"""
