8165dca4c38625f3b4915a0bbd5beaf6470a7d12
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-04-29 23:47:09 +0200

    v1.0 Acronym

--- What's my acronym?.py
+++ What's my acronym?.py
@@ -5,12 +5,14 @@ Input -> As Soon As Possible. Output -> ASAP.
 Input -> World Health Organization. Output -> WHO.
 Input -> Absent Without Leave. Output -> AWOL.
 """
-#fixing the bug for different letter issue
+# following code could be written in better way ,no time :)
 
 acronym = ""
 input_the_sentence = input("Please enter a sentence to generate Acronym: ")
 words = input_the_sentence.replace('Of', '').split()  # replace "Of" with space and then split list with space char
-print(words)
+
+if "of" in words:
+    words.remove("of")
 
 for i in words:
     acronym = i [ 0 ].upper() + acronym
