a7174166c2ba5125008759eb295af228545b8777
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-05-03 16:07:45 +0200

    v1.0 Rock Paper Scissors

--- Rock, Paper, Scissors.py
+++ Rock, Paper, Scissors.py
@@ -11,12 +11,25 @@ scissors (a fist with the index finger and middle finger extended, forming a V)
 
 import random  # will be used to input computer choice randomly
 
-user_action = input("Enter a choice (rock, paper, scissors): ") # our choice
-possible_actions = ["rock", "paper", "scissors"]  # all possible choices to be used for computer
-computer_action = random.choice(possible_actions) # creating computer action with random module ,possible actions
+user_action = input("Enter a choice (rock, paper, scissors): ")  # our choice
+possible_actions = [ "rock", "paper", "scissors" ]  # all possible choices to be used for computer
+computer_action = random.choice(possible_actions)  # creating computer action with random module ,possible actions
 # list will be used as argument
 
-print("\nYou are choosing {}".format(user_action),"computer is choosing {}\n".format(computer_action))
-
 if user_action == computer_action:
-    print("Both computer and user selected same action ,this is tie :) ")
+    print(f"Both players selected {user_action}. It's a tie!")
+elif user_action == "rock":
+    if computer_action == "scissors":
+        print("Rock smashes scissors! You win!")
+    else:
+        print("Paper covers rock! You lose.")
+elif user_action == "paper":
+    if computer_action == "rock":
+        print("Paper covers rock! You win!")
+    else:
+        print("Scissors cuts paper! You lose.")
+elif user_action == "scissors":
+    if computer_action == "paper":
+        print("Scissors cuts paper! You win!")
+    else:
+        print("Rock smashes scissors! You lose.")
