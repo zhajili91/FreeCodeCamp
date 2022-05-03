caa27ba07c0f374c7846e2865149acfb52174705
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-05-03 16:44:31 +0200

    v0.1 Guess the number

---
+++ Guess the number.py
@@ -0,0 +1,7 @@
+"""
+You ask a user to guess a number between 1 and 50.
+If they guess outside that range, you prompt with an error encouraging them to choose a number within the proper range.
+Whenever they guess the wrong number you ask if they want to keep playing or if they'd like to quit.
+Finally, when the user eventually guesses the right number you congratulate them and show the number of attempts they
+had.
+"""
